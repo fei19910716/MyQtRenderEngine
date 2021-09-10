efc4de501086c33e122a81f44b369e762cfca2c5
fordchen <fordchen@tencent.com>
2021-09-10 11:41:35 +0800

    适配mac

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 
 #include "mainwindow.h"
 #include <QApplication>
-
+#ifndef WIN32
+#include <QSurfaceFormat>
+#endif
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+#ifndef WIN32
+    QSurfaceFormat glFormat;
+    glFormat.setVersion(3,3);
+    glFormat.setProfile(QSurfaceFormat :: CoreProfile);
+    QSurfaceFormat :: setDefaultFormat(glFormat);
+#endif
     MainWindow w;
     w.show();
 
